bc23168b332eb1b375292ed6a51123c83369c8a0
Jesse Millar <hellojessemillar@gmail.com>
2016-11-06 13:47:25 -0700

    Cleanup and a start on database schemas

--- database/database.sql
+++ database/database.sql
@@ -1,5 +1,7 @@
+# Populated from https://github.com/jessemillar/american-dream-league/blob/master/database/dev/schema.txt
+
 DROP DATABASE IF EXISTS `american_dream_league`;
 CREATE DATABASE american_dream_league;
 USE american_dream_league;
 
-CREATE TABLE Players(id int(5));
+CREATE TABLE League(leagueID int(5), name varchar(25));
