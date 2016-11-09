c3984ae80baebc2b80027346a6761f2f48c7b114
dillhill51 <hamiltonhuskie5@aol.com>
2016-11-09 11:57:06 -0700

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -8,62 +8,25 @@ CREATE TABLE League(leagueID int(5), name varchar(25));
 
 CREATE TABLE User(userID int(5)), email varchar(25), password varchar(25), standing int(5));
 
-CREATE TABLE Email( emailID int(5), screenname varchar(25), domain varchar(25))
+CREATE TABLE Email( emailID int(5), screenname varchar(25), domain varchar(25));
 
-CREATE TABLE Password(passwordID int(5), hash)
+CREATE TABLE Password(passwordID int(5), hash);
 
-CREATE TABLE Friendship (friendshipID int(5), user int(5), user int(5))
+CREATE TABLE Friendship (friendshipID int(5), user int(5), user int(5));
 
-CREATE TABLE Team(
-  teamID,
-  mascot [mascotID],
-  logo [logoID],
-  name,
-  ownerName
-)
+CREATE TABLE Team(teamID int(5), mascot int(5), logo int(5), name varchar(25), ownerName varchar(25));
 
-CREATE TABLE Mascot(
-  mascotID,
-  name
-)
+CREATE TABLE Mascot(mascotID int(5), name varchar(25));
 
-CREATE TABLE Logo(
-  logoID,
-  file
-)
+CREATE TABLE Logo(logoID int(5), file varchar(25));
 
-CREATE TABLE Pitcher(
-  pitcherID,
-  saves,
-  innings,
-  WHIP,
-  ERA,
-  player [playerID]
-)
+CREATE TABLE Pitcher(pitcherID int(5), saves int(5), innings int(5), WHIP int(5), ERA int(5), player int(5));
 
-CREATE TABLE Batter(
-  batterID,
-  onBasePercent,
-  battingAverage,
-  strikeOuts,
-  slugPercent,
-  player [playerID],
-  hit [hitID],
-  atBat [atBatID]
-)
+CREATE TABLE Batter(batterID int(5), onBasePercent int(5), battingAverage int(5), strikeOuts int(5), slugPercent int(5), player int(5), hit int(5), atBat int(5))
 
-CREATE TABLE Hit(
-  hitID,
-  singles,
-  doubles,
-  triples,
-  homeRuns
-)
+CREATE TABLE Hit(hitID int(5), singles int(5), doubles int(5), triples int(5), homeRuns int(5))
 
-CREATE TABLE AtBat(
-  atBatID,
-  count
-)
+CREATE TABLE AtBat(atBatID int(5), count int(5))
 
 CREATE TABLE Player(playerID int(5),age int(5), gameCount int(5), wins int(5), name int(5), position int(5));
 
