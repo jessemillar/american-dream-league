f9319d033cfe622065f4c0943d8f884a649ba1fe
Jesse Millar <hellojessemillar@gmail.com>
2016-11-21 20:46:11 -0700

    Working SQL game generation triggers

--- database/database.sql
+++ database/database.sql
@@ -11,11 +11,30 @@ CREATE TABLE Mascots(ID int NOT NULL AUTO_INCREMENT, name varchar(500), PRIMARY 
 CREATE TABLE Teams(ID int NOT NULL AUTO_INCREMENT, name varchar(500), leagueID int, mascotID int, PRIMARY KEY (ID), FOREIGN KEY (leagueID) REFERENCES Leagues(ID), FOREIGN KEY (mascotID) REFERENCES Mascots(ID));
 CREATE TABLE Positions(ID int NOT NULL AUTO_INCREMENT, name varchar(500), PRIMARY KEY (ID));
 CREATE TABLE Players(ID int NOT NULL AUTO_INCREMENT, age int, nameID int, teamID int, positionID int, PRIMARY KEY (ID), FOREIGN KEY (teamID) REFERENCES Teams(ID), FOREIGN KEY (nameID) REFERENCES Names(ID), FOREIGN KEY (positionID) REFERENCES Positions(ID));
-CREATE TABLE Pitchers(ID int NOT NULL AUTO_INCREMENT, saves int, innings int, WHIP int, ERA int, playerID int, PRIMARY KEY (ID), FOREIGN KEY (playerID) REFERENCES Players(ID));
-CREATE TABLE Hitters(ID int NOT NULL AUTO_INCREMENT, onBasePercent int, battingAverage int, slugPercent int, playerID int, atBats int, PRIMARY KEY (ID), FOREIGN KEY (playerID) REFERENCES Players(ID));
-CREATE TABLE Hits(ID int NOT NULL AUTO_INCREMENT, singles int, doubles int, triples int, homeruns int, PRIMARY KEY (ID));
-CREATE TABLE AtBats(ID int NOT NULL AUTO_INCREMENT, playerID int, PRIMARY KEY (ID), FOREIGN KEY (playerID) REFERENCES Players(ID));
-CREATE TABLE Strikouts(ID int NOT NULL AUTO_INCREMENT, playerID int, PRIMARY KEY (ID), FOREIGN KEY (playerID) REFERENCES Players(ID));
+CREATE TABLE Hitters(ID int NOT NULL AUTO_INCREMENT, playerID int, homeruns int, runsBattedIn int, battingAverage float(7,4), PRIMARY KEY (ID), FOREIGN KEY (playerID) REFERENCES Players(ID));
+CREATE TABLE Pitchers(ID int NOT NULL AUTO_INCREMENT, playerID int, strikeouts int, wins int, earnedRunAverage float(7,4), PRIMARY KEY (ID), FOREIGN KEY (playerID) REFERENCES Players(ID));
+
+delimiter //
+CREATE TRIGGER SimulateGameHitter BEFORE INSERT ON Hitters
+FOR EACH ROW 
+BEGIN
+	# 0-2 homeruns, 0-10 runsBattedIn, 0.1-0.4 battingAverage
+	SET NEW.homeruns = FLOOR(RAND()*2);
+	SET NEW.runsBattedIn = FLOOR(RAND()*11);
+	SET NEW.battingAverage = RAND()*(0.4-0.1)+0.1;
+END; //
+delimiter ;
+
+delimiter //
+CREATE TRIGGER SimulateGamePitcher BEFORE INSERT ON Pitchers
+FOR EACH ROW 
+BEGIN
+	# 0-20 strikeouts, 0-1 wins, 0.5-4 earnedRunAverage
+	SET NEW.strikeouts = FLOOR(RAND()*20);
+	SET NEW.wins = FLOOR(RAND()*1);
+	SET NEW.earnedRunAverage = RAND()*(4-0.5)+0.5;
+END; //
+delimiter ;
 
 INSERT INTO Emails(username,domain) VALUES ("test1","gmail.com");
 INSERT INTO Emails(username,domain) VALUES ("test2","gmail.com");
